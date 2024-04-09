3b6d5796d4f84f28f70ace69be76988c237f07cc
cropsun <martinsunshinesh@gmail.com>
2024-04-09 22:15:25 +0800

    new fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aivox",
-    version="0.1.0",
+    version="0.0.1",
     author="Cropsun",
     author_email="martinsunshinesh@gmail.com",
     description="A simple API client for AI-Music Generate",
